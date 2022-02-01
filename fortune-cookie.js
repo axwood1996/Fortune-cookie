ad19440a27f1f2b93650428aac788b0dd2cf8b3e
axelwood <axelwood1996@gmail.com>
2022-02-01 20:05:09 -0300

    finished

--- fortune-cookie.js
+++ fortune-cookie.js
@@ -1,6 +1,6 @@
 let nouns =['Love','Fear','Incompetence', 'Dishonesty', 'Joy', 'Happiness', 'Fullfilment', 'Anger', 'Death', 'New beginnings','Money', 'Goals', 'Work', 'Companionship', 'A project', 'Family', 'Unexpected Guest']
 let pointers=[' is around the corner. ',' is expected. ', ' is a clucth. ' ,' will not be in your life for now. ', ' will present itself. ', ' will reach out. ',' will drop into you life. ' , ' will meddle with your goals. ' , ' will guide you. ', ' will derail your journey. ' ];
-let cautionary = ['Caution!' , 'Embrace it.', 'Expect turmoil.', 'Beware!', 'Deal with it!', 'Push through.', 'Hold on!', 'Find balance', 'Consult it with friends']
+let cautionary = ['Caution! ' , 'Embrace it. ', 'Expect turmoil. ', 'Beware! ', 'Deal with it! ', 'Push through. ', 'Hold on! ', 'Find balance. ', 'Consult it with friends. ']
 
 
 const fortunecookie = (array1 , array2, array3) =>{
@@ -9,6 +9,7 @@ const fortunecookie = (array1 , array2, array3) =>{
     let cautionary = array3[Math.floor(Math.random() * array3.length)]
     let fortune = ''
     fortune += noun + pointer + cautionary
+    fortune += `Your lucky numbers are ${Math.floor(Math.random()*100)}, ${Math.floor(Math.random()*100)} and ${Math.floor(Math.random()*100)}.`
     return fortune
     
 }
